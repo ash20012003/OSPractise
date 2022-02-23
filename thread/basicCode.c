e923ee1715ee613614955e31cc445c4ac8ddbbd7
Ashwath Vasudevan <83507067+ash20012003@users.noreply.github.com>
2022-02-23 10:41:55 +0530

    Create basicCode.c

---
+++ thread/basicCode.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *printHello(){
+  printf("Hello all\n");
+  sleep(3);
+  printf("Thread ended");
+}
+
+int main(int argc, char* argv[]){
+  pthread_t t1;
+  pthread_t t2;
+  pthread_create(t1,NULL,&printHello,NULL);
+  pthread_create(t1,NULL,&printHello,NULL);
+  pthread_join(t1,NULL);
+  pthread_join(t1,NULL);  
+  return 0;
+}
