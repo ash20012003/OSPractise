bc249fa32af94a8ec9eca3ffe8af36a448c90970
Ashwath Vasudevan <83507067+ash20012003@users.noreply.github.com>
2022-02-23 10:44:56 +0530

    Update basicCode.c

--- thread/basicCode.c
+++ thread/basicCode.c
@@ -13,9 +13,9 @@ void *printHello(){
 int main(int argc, char* argv[]){
   pthread_t t1;
   pthread_t t2;
-  pthread_create(t1,NULL,&printHello,NULL);
-  pthread_create(t1,NULL,&printHello,NULL);
+  pthread_create(&t1,NULL,&printHello,NULL);
+  pthread_create(&t2,NULL,&printHello,NULL);
   pthread_join(t1,NULL);
-  pthread_join(t1,NULL);  
+  pthread_join(t2,NULL);  
   return 0;
 }
